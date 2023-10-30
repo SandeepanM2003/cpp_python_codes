1c0460141da96d4748d6a137ce8f6a14630e96fc
Aira Jena <113255984+0xaira@users.noreply.github.com>
2023-10-30 21:45:17 +0530

    Added Morris Preorder Traversal of a Binary Tree

---
+++ C++/Morris Preorder Traversal.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct node {
+  int data;
+  struct node * left, * right;
+};
+
+vector < int > preorderTraversal(node * root) {
+  vector < int > preorder;
+
+  node * cur = root;
+  while (cur != NULL) {
+    if (cur -> left == NULL) {
+      preorder.push_back(cur -> data);
+      cur = cur -> right;
+    } else {
+      node * prev = cur -> left;
+      while (prev -> right != NULL && prev -> right != cur) {
+        prev = prev -> right;
+      }
+
+      if (prev -> right == NULL) {
+        prev -> right = cur;
+        preorder.push_back(cur -> data);
+        cur = cur -> left;
+      } else {
+        prev -> right = NULL;
+        cur = cur -> right;
+      }
+    }
+  }
+  return preorder;
+}
+
+struct node * newNode(int data) {
+  struct node * node = (struct node * ) malloc(sizeof(struct node));
+  node -> data = data;
+  node -> left = NULL;
+  node -> right = NULL;
+
+  return (node);
+}
+
+int main() {
+
+  struct node * root = newNode(1);
+  root -> left = newNode(2);
+  root -> right = newNode(3);
+  root -> left -> left = newNode(4);
+  root -> left -> right = newNode(5);
+  root -> left -> right -> right = newNode(6);
+
+  vector < int > preorder;
+  preorder = preorderTraversal(root);
+
+  cout << "The Preorder Traversal is: ";
+  for (int i = 0; i < preorder.size(); i++) {
+    cout << preorder[i] << " ";
+  }
+
+  return 0;
+}
